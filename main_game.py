303407766a88f809160fa7584c17bebabb0c9c4c
DanMaker01 <danmaker01@gmail.com>
2024-07-22 14:55:25 -0300

    adicionado objetivos do projeto

--- main_game.py
+++ main_game.py
@@ -1,3 +1,14 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # 
+# Luta Turno
+# https://github.com/danmaker01/luta_turno
+# danmaker01
+# 2024
+# # # # # # # # # # # # # # # # # # # # # # # # # 
+# Jogo de luta em turno ABS
+# O objetivo é passar a sensação do ritmo de uma luta
+# com movimentação, ataque e defesa, antecipação e gingado.
+
+
 import pygame
 import sys
 from player import Player
